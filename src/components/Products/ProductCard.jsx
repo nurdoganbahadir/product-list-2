70967ab211abc8492d85b950e31f4788a41320ba
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-09-02 16:47:46 +0300

    porduct list ve product cardları ayalandı

--- src/components/Products/ProductCard.jsx
+++ src/components/Products/ProductCard.jsx
@@ -1,9 +1,25 @@
 import "./ProductCard.scss";
-
+import Col from "react-bootstrap/Col";
 import React from "react";
+import Card from "react-bootstrap/Card";
+import { MdFavorite } from "react-icons/md";
 
-const ProductCard = () => {
-  return <div className="product">ProductCard</div>;
+const ProductCard = ({ item }) => {
+  return (
+    <Col xs={12} md={6} lg={4} xl={3}>
+      <Card className="rounded-2 m-auto card" role="button">
+        <Card.Header className="d-flex justify-content-between">
+          <Card.Title>{item.price} $</Card.Title>
+          <MdFavorite size={30} />
+        </Card.Header>
+        <Card.Img variant="top" src={item.image} className="player-logo" />
+
+        <Card.Footer className="card__over">
+          <Card.Title>{item.title}</Card.Title>
+        </Card.Footer>
+      </Card>
+    </Col>
+  );
 };
 
 export default ProductCard;
--- src/components/Products/ProductList.jsx
+++ src/components/Products/ProductList.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import { products } from "../../helper/data";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import ProductCard from "./ProductCard.jsx";
 
 const ProductList = () => {
   return (
-    <div>ProductList</div>
-  )
-}
+    <>
+      <Container className="product-list rounded-4 my-4 p-3">
+        <Row className="g-3 justify-content-center">
+          {products.map((item) => (
+            <ProductCard item={item} />
+          ))}
+        </Row>
+      </Container>
+    </>
+  );
+};
 
-export default ProductList
+export default ProductList;
