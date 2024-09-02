eb2beee5a5c8065e8f1d2364b9f0a8cb7db93c22
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-09-02 16:08:20 +0300

    scss'ler hazırlandı header search input eklendi

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import "./Header.scss";
 import { categories } from "../../helper/data";
 import React from "react";
+import Form from "react-bootstrap/Form";
 
 const Header = () => {
   return (
@@ -11,6 +12,11 @@ const Header = () => {
           <button>{item}</button>
         ))}
       </div>
+      <Form.Control
+        type="text"
+        placeholder="Search Product"
+        className=" mr-sm-2 my-4 w-50 m-auto"
+      />
     </div>
   );
 };
