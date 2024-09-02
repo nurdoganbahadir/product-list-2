21a2184a1cddb5e62862afe5ac071e2bd28c3270
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-09-02 16:16:00 +0300

    product list starter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.scss";
 import Header from "./components/Header/Header";
+import ProductList from "./components/Products/ProductList";
 
 function App() {
   return (
     <>
       <Header />
+      <ProductList />
     </>
   );
 }
