202e7fccef7252889124b6931eb5a2df6a92fe16
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-09-03 00:38:55 +0300

    kalp iconu eklendi çalışıyor

--- src/components/Products/ProductCard.jsx
+++ src/components/Products/ProductCard.jsx
@@ -1,16 +1,25 @@
 import "./ProductCard.scss";
 import Col from "react-bootstrap/Col";
-import React from "react";
+import React, { useState } from "react";
 import Card from "react-bootstrap/Card";
 import { MdFavorite } from "react-icons/md";
 
 const ProductCard = ({ item, key }) => {
+  const [liked, setLiked] = useState(false);
+
+  const handleLikeClick = () => {
+    setLiked(!liked);
+  };
   return (
     <Col xs={12} md={6} lg={4} xl={3} key={key}>
       <Card className="rounded-2 m-auto card" role="button">
         <Card.Header className="d-flex justify-content-between">
           <Card.Title>{item.price} $</Card.Title>
-          <MdFavorite size={30} />
+          <MdFavorite
+            size={30}
+            className={`heart-icon ${liked ? "active" : ""}`}
+            onClick={handleLikeClick}
+          />
         </Card.Header>
         <Card.Img variant="top" src={item.image} className="player-logo" />
 
